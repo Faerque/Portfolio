f2dfb9b5b83fa6533bb59b038869a08f63f34988
Faerque <202001512@eastdelta.edu.bd>
2021-05-03 00:36:24 +0600

    more change

--- src/components/Home/Skills/Skills.js
+++ src/components/Home/Skills/Skills.js
@@ -11,8 +11,8 @@ import Fade from 'react-reveal/Fade';
 
 const Skills = () => {
   return (
-    <div className="container container-sm">
-      <div className="row">
+    <div className="container container-fluid">
+      <div className="">
         <div className="text-white mt-5 ">
           <h3>
             I am <span style={{ color: "#0DFCDC" }}> Experts </span>on:
@@ -20,40 +20,44 @@ const Skills = () => {
           <hr />
         </div>
         <Fade bottom cascade>
+        <div className="container">
+        <div className="row my-4">
         <div
           style={{ padding: "" }}
           className="col-2 text-center text-white mb-3"
         >
-          <FontAwesomeIcon icon={faHtml5} size="4x" />
-          <p className="m-1 "> HTML5 </p>
+          <FontAwesomeIcon icon={faHtml5} size="3x" />
+          <p className="m-2"> HTML5 </p>
         </div>
         <div
           style={{ padding: "" }}
-          className="col-2 mx-auto text-center text-white mb-3"
+          className="col-2  text-center text-white mb-3"
         >
-          <FontAwesomeIcon icon={faCss3} size="4x" />
-          <p className="m-1"> CSS3 </p>
+          <FontAwesomeIcon icon={faCss3} size="3x" />
+          <p className="m-2"> CSS3 </p>
         </div>
         <div
           style={{ padding: "" }}
-          className="col-2 mx-auto text-center text-white mb-3"
+          className="col-2 text-center  text-white mb-3"
         >
-          <FontAwesomeIcon icon={faJsSquare} size="4x" />
-          <p className="m-1"> JavaScript </p>
+          <FontAwesomeIcon icon={faJsSquare} size="3x" />
+          <p className="m-2"> <small>JS</small> </p>
         </div>
         <div
           style={{ padding: "" }}
-          className="col-2 mx-auto text-center text-white mb-3"
+          className="col-2 text-center text-white mb-3"
         >
-          <FontAwesomeIcon icon={faReact} size="4x" />
-          <p className="m-1"> React.js </p>
+          <FontAwesomeIcon icon={faReact} size="3x" />
+          <p className="m-2">React.js </p>
         </div>
         <div
           style={{ padding: "" }}
-          className="col-md-2 mx-auto text-center text-white mb-3"
+          className="col-2 text-center text-white mb-3"
         >
-          <FontAwesomeIcon icon={faNode} size="4x" />
-          <p className="m-1"> Node.js </p>
+          <FontAwesomeIcon icon={faNode} size="3x" />
+          <p className="m-2"> Node.js </p>
+        </div>
+        </div>
         </div>
         </Fade>       
       </div>
